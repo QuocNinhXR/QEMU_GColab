7ec21978ae8ee184688e6945cf1057bb958c7f66
QuocNinhXR <104149541+QuocNinhXR@users.noreply.github.com>
2022-07-19 18:28:36 +0700

    Update and rename ngroksetup.sh to OnVMAg.sh

--- ngroksetup.sh
+++
@@ -1,5 +0,0 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-rm -rf ngrok-stable-linux-amd64.zip
-./ngrok authtoken 1sv5vgXWhbhBNbzxL80cPVV3Aom_4Cd1Z2hyTHQ8dicpqy5D5
-nohup ./ngrok tcp --region ap 5900 &>/dev/null &
